1029415aa961100b0ec829e6b0d9ed0708c7c4d9
Zuu97 <isurualagiyawanna9717@gamil.com>
2020-10-30 13:59:24 +0530

    # object detection model

--- webcam.py
+++ webcam.py
@@ -2,6 +2,8 @@ import cv2 as cv
 import numpy as np
 from PIL import Image
 from face_mask_detection import FaceMaskModel
+from detect import object_detection
+from cv2 import CascadeClassifier
 
 from variables import*
 
@@ -12,21 +14,24 @@ if __name__ == "__main__":
 
         while True:
                 _, frame = video.read()
-
+                frame = cv.flip(frame, 1)
                 im = Image.fromarray(frame, 'RGB')
                 img_array = np.array(im)
-                img_array = cv.resize(img_array, target_size).astype("float32") 
-                img_array = img_array * rescale
+                img_array = cv.resize(img_array, target_size)
+                img = object_detection(CascadeClassifier, img_array)
+
+                img_array = img_array.astype("float32")  * rescale
                 assert (img_array.shape == input_shape), "corrupted image"
 
                 img_array = np.array([img_array])
                 pred = model.predition(img_array)
 
+                img = cv.resize(img, (800, 500))
                 text = "Status : {}".format(pred)
-                cv.putText(frame, text, (35, 50), cv.FONT_HERSHEY_SIMPLEX,
+                cv.putText(img, text, (35, 50), cv.FONT_HERSHEY_SIMPLEX,
                                 1.25, (0, 255, 0), 5)
 
-                cv.imshow("Capturing", frame)
+                cv.imshow("Capturing", img)
                 key=cv.waitKey(1)
                 if key == ord('q'):
                         break
